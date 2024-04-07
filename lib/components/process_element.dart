6dca07b244e6dccab9f45b60e7ffbcb08e85681a
Mohamed Atef <atefm1906@gamil.com>
2024-04-07 21:18:00 +0200

    2

--- lib/components/process_element.dart
+++ lib/components/process_element.dart
@@ -7,32 +7,37 @@ class ProcessElement extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        decoration: BoxDecoration(
-            color: const Color.fromARGB(255, 240, 235, 235),
-            borderRadius: BorderRadius.circular(30)),
-        height: 50,
-        width: double.infinity,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(
-            children: [
-              const Spacer(
-                flex: 1,
-              ),
-              Text(
-                txt,
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20
+    return InkWell(
+      onTap: (){
+        
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 240, 235, 235),
+              borderRadius: BorderRadius.circular(30)),
+          height: 50,
+          width: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                const Spacer(
+                  flex: 1,
                 ),
-              ),
-              const Spacer(
-                flex: 4,
-              ),
-            ],
+                Text(
+                  txt,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20
+                  ),
+                ),
+                const Spacer(
+                  flex: 4,
+                ),
+              ],
+            ),
           ),
         ),
       ),
